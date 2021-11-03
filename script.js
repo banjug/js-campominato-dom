13c76ca964ba1dba176f7d43548f3a5adb5e4463
Samuele <miotto.samuele@gmail.com>
2021-11-03 11:29:14 +0100

    pseudocode

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 /*
-
+// ------------------------------------------------------------
+PRIMA PARTE----------------------------------------------------
 1. far selezionare la difficoltà -> promt/pulsanti
     1.1 a seconda della difficoltà generare la griglia (facile 10x10, medio 9x9, difficile 7x7) -> if con tre funzioni di generazione degli elementi
 2. selezionata la difficoltà generare la griglia
@@ -9,6 +10,16 @@
     2.4 generare il numero corretto di quadrati all'interno della griglia a seconda della difficoltà
     (tutto questo sta in una funzione - la posso richiamare al click o inserendo la difficoltà nel prompt)
 3. al clic del giocatore cambiare il colore del singolo quadratino -> aggiungere classe active che cambia lo sfondo con addEventListener
+
+// ------------------------------------------------------------
+SECONDA PARTE--------------------------------------------------
+1.generare 16 numeri casuali che vanno a rappresentare le bombe
+    1.1 controllare che i numeri non siano duplicati
+2.marchiare le celle con uno dei numeri generati come bomba
+3.se clicco su una bomba imposto un diverso colore
+4.contare il numero di click corretti prima di cliccare una bomba (punteggio)
+5.il gioco finisce se clicco su una bomba o se scelgo tutte le caselle senza bomba
+6.quando clicco su una bomba mostrare tutte le caselle contententi una bomba
 
 */
 
@@ -77,7 +88,7 @@ function gridDifficulty(x, y) {
 
 // al click cambia lo sfondo dei quadrati e aggiunge il numero all'interno
 function squareClickAdd(x, y) {
-    let squareClick = document.querySelectorAll(x);
+    let squareClick = document.querySelectorAll(x);    
         console.log(squareClick);
 
         for (let i = 0; i < squareClick.length; i++) {
